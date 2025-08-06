be0af56c2a7842a971375aa187ce434adb7a106e
atharva-dharankar0007 <atharvadharankar0007@gmail.com>
2025-08-06 22:44:43 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,9 @@
+# 5 aug 2025
+# Assignment1
+# Accept Radius from user and Display area of circle
+# Formula -> area = 3.14* radius * radius
+
+
+radius=float(input("Enter the Radius of circle:"))
+circle_area=3.14*radius*radius
+print(f"The area for diven radius {radius} is {circle_area}")
